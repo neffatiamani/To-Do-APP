0b371c14c92395db6f9e17fb6b7f3b0565db0639
ameni neffati <neffatiamani14@gmail.com>
2023-05-09 15:52:32 +0100

    build an image

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, request, session, redirect
 from flask_mysqldb import MySQL
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
-app.secret_key = "1025"  # Replace with a secure key
+app.secret_key = os.environ.get("SECRET_KEY", "1025")
+load_dotenv()
+
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Lina&mimi2706'  # Replace with your MySQL password
+app.config['MYSQL_PASSWORD'] = 'Lina&mimi2706'
 app.config['MYSQL_DB'] = 'mydatabase'
-mysql = MySQL(app)
 
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
@@ -34,29 +38,82 @@ def login():
         cur.close()
         if user:
             session['username'] = username
-            session['id'] = user[1]
+            session['id'] = user[0]
             return redirect('/')
         else:
             return render_template('login.html', error='Invalid login')
     else:
         return render_template('login.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        if password != confirm_password:
+            return render_template('register.html', error='Passwords do not match')
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE username=%s", [username])
+        user = cur.fetchone()
+        if user:
+            return render_template('register.html', error='Username already taken')
+        else:
+            cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+            mysql.connection.commit()
+            cur.close()
+            return redirect('/login')
+    else:
+        return render_template('register.html')
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
-    session.pop('user_id', None)
+    session.pop('id', None)
     return redirect('/')
+
+@app.route('/profile')
+def profile():
+    if 'username' in session:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE id=%s", [session['id']])
+        user = cur.fetchone()
+        cur.close()
+        return render_template('profile.html', user=user)
+    else:
+        return redirect('/login')
 
 
 @app.route('/add', methods=['POST'])
 def add_task():
-    task = request.form['task']
-    cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO tasks (user_id, task) VALUES (%d, %s)", (int(session['user_id']), task))
-    mysql.connection.commit()
-    cur.close()
+    if request.method == 'POST':
+        task = request.form['task']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO tasks (user_id, task) VALUES (%s, %s)", (session['id'], task))
+        mysql.connection.commit()
+        cur.close()
     return redirect('/')
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+def edit_profile():
+    if 'username' in session:
+        if request.method == 'POST':
+            name = request.form['name']
+            email = request.form['email']
+            cur = mysql.connection.cursor()
+            cur.execute("UPDATE users SET name=%s, email=%s WHERE id=%s", (name, email, session['id']))
+            mysql.connection.commit()
+            cur.close()
+            return redirect('/profile')
+        else:
+            cur = mysql.connection.cursor()
+            cur.execute("SELECT * FROM users WHERE id=%s", [session['id']])
+            user = cur.fetchone()
+            cur.close()
+            return render_template('edit_profile.html', user=user)
+    else:
+        return redirect('/login')
+
 
 
 if __name__ == '__main__':
